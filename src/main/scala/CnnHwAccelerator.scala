57aedadd6f427e5422394900f65c86db23919bab
Owen Sowatzke <osowatzke@gmail.com>
2024-12-03 21:26:30 -0700

    Adding alignment buffer for data out of RAM

--- src/main/scala/CnnHwAccelerator.scala
+++ src/main/scala/CnnHwAccelerator.scala
@@ -159,8 +159,8 @@ class CnnHwAcceleratorClient(beatBytes: Int)(implicit p: Parameters) extends Laz
             p.vectorSize,
             p.maxSize)
 
-        val readFifo    = Module(new Fifo(UInt((addrBits + blockBytes).W), 8, 1))
-        val writeFifo   = Module(new Fifo(UInt((dataBits + addrBits + blockBytes + 1).W), 8, 1))
+        val readFifo    = Module(new Fifo(UInt((addrBits + blockBytes).W), 8, 2))
+        val writeFifo   = Module(new Fifo(UInt((dataBits + addrBits + blockBytes + 1).W), 8, 2))
         
         switch (readSelStateR) {
             is (ReadSelState.ReadData) {
@@ -211,9 +211,36 @@ class CnnHwAcceleratorClient(beatBytes: Int)(implicit p: Parameters) extends Laz
         readAddr2R  := readAddrNext2R
 
         readValid3R := readValid2R
+        readDone3R  := readDone2R
         readAddr3R  := Cat(readAddr2R(addrBits-1, blockBytesLog2), 0.U(blockBytesLog2))
         readMask3R  := getMask(readAddr2R, readSize2R)
 
+        readFifo.io.enq.valid = readValid3R
+        readFifo.io.enq.bits  = Cat(readDone3R, readMask3R, readAddr3R)
+
+        readFifo.io.deq.ready = arbFifo.io.enq.ready && !writeFifo.io.deq.valid
+        writeFifo.io.deq.ready = arbFifo.io.enq.ready
+
+        arbValidR := (writeFifo.io.deq.valid || readFifo.io.deq.valid) && arbFifo.io.enq.ready
+
+        val tl_a_data = WireInit(0.U(dataBits.W))
+        val tl_a_addr = WireInit(0.U(addrBits.W))
+        val tl_a_mask = WireInit(0.U(blockBytes.W))
+
+        when (writeFifo.io.deq.valid) {
+        
+        } .otherwise {
+            tl_a_addr   := readFifo.io.deq.bits()
+            arbDataR    := edge.Get(0.U, tl_a_addr, log2Ceil(blockBytes).U)._2
+        }
+
+        arbDataR := 
+
+        switch (arbStateR) {
+            when (readFifo.io.deq.valid) {
+                
+            }
+        }
 
         switch (readStateR)
         {
@@ -241,6 +268,8 @@ class CnnHwAcceleratorClient(beatBytes: Int)(implicit p: Parameters) extends Laz
 
         }
 
+        when ()
+
         when (tl.a.fire && !tl.d.fire) {
             cntR        := cntR + 1
         } .elsewhen (!t1.a.fire && tl.d.fire) {
@@ -256,7 +285,181 @@ class CnnHwAcceleratorClient(beatBytes: Int)(implicit p: Parameters) extends Laz
         }
 
 
+
+        for (i <- 0 to (blockBytes-1))
+        {
+            val byteFifo             = Module(new Fifo(UInt(8.W), 8, 2))
+            byteFifo.io.enq.valid   := tlFifoWrEnVecR(i) && tl_d_validR
+            byteFifo.io.enq.bits    := (tlFifoWrDataR >> (8 * i))(7, 0)
+            tlFifoWrReady(i)        := byteFifo.io.enq.ready
+
+            byteFifoRdValid(i)      := byteFifo.io.deq.valid
+            byteFifo.io.deq.ready   := byteFifoRdReady(i)
+        }
+
+        bytesLeftR      := bytesLeftR - beatBytes
+        when (bytes)
+        when (bytesLeftR <= beatBytes) {
+            readStateR  :=  doneR
+        }
+
+
+        byteFifoRdReady := (byteFifoRdValid.asUInt === rdMaskR) ? rdMaskR : 0
+
+        ramWrEnR                    := byteFifoRdReady
+        ramWrDataR                  := byteFIfoRdData.asUInt
+
+        when (byteFifoRdValid.asUInt === rdMaskR) {
+            rdMaskR                 := rdMaskR >> 
+        }
+        
+        when () (ramWrEnR)
+        ramAddrR                    := 
+
+        byFifoWrReadyR              := (byteFifoWrReady.asUInt === -1.S(blockBytes.W).asUInt)
+        byteFifoRdDataR             := byteFifoRdData.asUInt
+
+        when (tlFifo)
+        switch(writeStateR) {
+            is (WriteState.Init) {
+                addrR               := 0
+            }
+            is (WriteState.Wait) {
+                when (byteFifoRdReady == ) {
+
+                }
+                addrR               := addrR + 1
+            }
+        }
+
+
     }
 }
 
-class ReadQueue()
+
+class AlignmentBuffer(addrWidth: Int, beatBytes: Int) extends Module
+{
+
+    val dataWidth  = 8 * beatBytes
+    val countWidth = addrWidth + log2Ceil(beatBytes) + 1
+
+    val io = IO(new Bundle{
+        val startIn    = Input(Bool())
+        val sizeIn     = Input(UInt((addrWidth+1).W))
+        val wrValidIn  = Input(UInt(beatBytes.W))
+        val wrDataIn   = Input(UInt(dataWidth.W))
+        val wrReadyOut = Output(Bool())
+        val wrEnOut    = Output(UInt(beatBytes.W))
+        val wrDataOut  = Output(UInt(beatBytes.W))
+        val addrOut    = Output(UInt(addrWidth.W))
+        val lastOut    = Output(Bool())
+    })
+
+    def getMask(bytesLeft : UInt) : UInt {
+        val mask = WireInit(0.U(beatBytes.W))
+        when (bytesLeft >= beatBytes) {
+            mask := -1.S(beatBytes.W).asUInt
+        } .otherwise {
+            for (i <- 0 to (blockBytes-1)) {
+                when (bytesLeft >= i.U)
+                {
+                    mask(i) := true.B
+                }
+            }
+        }
+        return mask
+    }
+    
+    object State extends ChiselEnum
+    {
+        val Init, Read = Value
+    }
+
+    val sizeBytes    = WireInit(0.U(countWidth.W))
+    sizeBytes       := Cat(io.sizeIn, 0.U(log2Ceil(beatBytes).W))
+
+    val ctrlFifo     = Module(new Fifo(UInt((beatBytes+addrWidth+1).W), 8, 1))
+
+    validR           = RegInit(false.B)
+    stateR           = RegInit(State.Init)
+    bytesLeftR       = Reg(UInt(countWidth.W))
+    nextAddrR        = Reg(UInt(addrWidth.W))
+
+    validR          := false.B
+    switch (stateR) {
+        is (State.Init) {
+            when (io.startIn) {
+                bytesLeftR  := sizeBytes
+                nextAddrR   := 0
+                stateR      := State.Read
+            }
+        }
+        is (State.Read) {
+            when (ctrlFifo.io.enq.ready) {
+                validR      := true.B
+                bytesLeftR  := bytesLeftR - beatBytes
+                nextAddrR   := nextAddrR + beatBytes
+                when (bytesLeftR <= beatBytes) {
+                    stateR  := State.Init
+                }
+            }
+        }
+    }
+
+    addrR    = Reg(UInt(addrWidth.W))
+    maskR    = Reg(UInt(beatBytes.W))
+    lastR    = Reg(Bool())
+
+    addrR   := nextAddrR
+    maskR   := getMask(bytesLeftR)
+    lastR   := bytesLeftR <= beatBytes ? true.B : false.B
+    
+    ctrlFifo.io.enq.bits    := Cat(lastR, maskR, addrR)
+    ctrlFifo.io.enq.valid   := validR
+
+    val addrLo      = 0
+    val addrHi      = addrLo + addrWidth - 1
+    val maskLo      = addrHi + 1
+    val maskHi      = maskLo + beatBytes - 1
+    val lastIdx     = maskHi + 1
+    
+    byteFifoWrReady = WireInit(0.U(beatBytes.W))
+    byteFifoRdValid = WireInit(0.U())
+    byteFifoRdData  = WireInit(Seq.fill(blockBytes)(0.U(8.W)))
+
+    for (i <- 0 to (blockBytes-1)) {
+        val byteFifo             = Module(new Fifo(UInt(8.W), 8, 2))
+        byteFifo.io.enq.valid   := io.wrValidIn(i)
+        byteFifo.io.enq.bits    := (io.wrDataIn >> (8 * i))(7, 0)
+        byteFifoWrReady(i)      := byteFifo.io.enq.valid
+        byteFifo.io.deq.ready   := byteFifoRdReady(i) 
+        byteFifoRdValid(i)      := byteFifo.io.deq.valid
+        byteFifoRdData(i)       := byteFifo.io.deq.bits
+    }
+    
+    wrReadyR         = RegInit(false.B)
+    wrReadyR        := (byteFifoWrReady === -1.S(beatBytes).asUInt)
+
+    io.wrReadyOut   := wrReadyR
+
+    rdReady          = WireInit(false.B)
+    rdReady         := ctrlFifo.io.deq.valid & (ctrlFifo.io.deq.bits === byteFifoRdValid)
+
+    byteFifoRdReady  = WireInit(false.B)
+    byteFifoRdReady := rdReady ? ctrlFifo.io.deq.bits(maskHi, maskLo) : 0.U
+
+    wrEnOutR         = RegInit(0.U(beatBytes.W))
+    wrDataOutR       = Reg(UInt(dataWidth.W))
+    addrOutR         = Reg(UInt(addrWidth.W))
+    lastOutR         = RegInit(false.B)
+
+    wrEnOutR        := byteFifoRdReady
+    wrDataOutR      := byteFifoRdData.asUInt
+    addrOutR        := ctrlFifo.io.deq.bits(maskHi, maskLo)
+    lastOutR        := ctrlFifo.io.deq.bits(lastIdx)
+    
+    wrEnOut         := wrEnOutR
+    wrDataOut       := wrDataOutR
+    addrOut         := addrOutR
+    lastOut         := lastOutR
+}
